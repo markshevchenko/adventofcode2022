47011f159b19617dcbb3cd140c718c4a9e3e56d8
Mark Shevchenko <markshevchenko@gmail.com>
2022-12-04 02:26:54 +0300

    Solve problem 3 in F#

---
+++ fsharp-tests/Problem03Tests.fs
@@ -0,0 +1,41 @@
+﻿module Problem03Tests
+
+open Xunit
+open Utils
+open Problem03
+
+
+[<Fact>]
+let ``priority returns valid values`` () =
+    Assert.Equal (1, priority 'a')
+    Assert.Equal (26, priority 'z')
+    Assert.Equal (27, priority 'A')
+    Assert.Equal (52, priority 'Z')
+
+
+[<Fact>]
+let ``solve_a with sample returns 157`` () =
+    let sample = "vJrwpWtwJgWrhcsFMMfFFhFp\n\
+                  jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\n\
+                  PmmdzqPrVvPwwTWBwg\n\
+                  wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\n\
+                  ttgJtRGJQctTZtZT\n\
+                  CrZsJsPPZsGzwwsLwLmpwMDw\n"
+    
+    let actual = solve_a (sample |> String.to_reader |> Seq.from_reader) 
+                  
+    Assert.Equal(157, actual)
+    
+    
+[<Fact>]
+let ``solve_b with sample returns 70`` () =
+    let sample = "vJrwpWtwJgWrhcsFMMfFFhFp\n\
+                  jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\n\
+                  PmmdzqPrVvPwwTWBwg\n\
+                  wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\n\
+                  ttgJtRGJQctTZtZT\n\
+                  CrZsJsPPZsGzwwsLwLmpwMDw\n"
+    
+    let actual = solve_b (sample |> String.to_reader |> Seq.from_reader) 
+                  
+    Assert.Equal(70, actual)
---
+++ fsharp/Problem03.fs
@@ -0,0 +1,32 @@
+﻿module Problem03
+
+open Microsoft.FSharp.Collections
+
+let priority c =
+    if c >= 'a'
+    then int c - int 'a' + 1
+    else int c - int 'A' + 27
+
+
+let solve_a (lines: string seq) =
+    let mutable result = 0
+    for line in lines do
+        let left_half = Set.ofSeq line[..line.Length/2 - 1] 
+        let right_half = Set.ofSeq line[line.Length/2..]
+        let share_item = Set.intersect left_half right_half |> Seq.exactlyOne
+        
+        result <- result + priority share_item
+        
+    result
+
+
+let solve_b (lines: string seq) =
+    let mutable result = 0
+    for triple in Seq.chunkBySize 3 lines do
+        let share_item = triple |> Seq.map Set.ofSeq
+                                |> Set.intersectMany
+                                |> Seq.exactlyOne
+        
+        result <- result + priority share_item
+        
+    result
