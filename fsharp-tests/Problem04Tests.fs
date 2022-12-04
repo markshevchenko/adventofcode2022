41e138afa22cba02ffd0ef294eb5680e964c64f4
Mark Shevchenko <markshevchenko@gmail.com>
2022-12-04 10:47:16 +0300

    Solve problem 4 in F#

---
+++ fsharp-tests/Problem04Tests.fs
@@ -0,0 +1,37 @@
+﻿module Problem04Tests
+
+open Xunit
+open Utils
+open Problem04
+
+[<Fact>]
+let ``parse "2-3,7-8" returns ((2, 3), (7, 8))`` () =
+    Assert.Equal (((2, 3), (7, 8)), parse "2-3,7-8")
+    
+
+[<Fact>]
+let ``solve_a with sample returns 2`` () =
+    let sample = "2-4,6-8\n\
+                  2-3,4-5\n\
+                  5-7,7-9\n\
+                  2-8,3-7\n\
+                  6-6,4-6\n\
+                  2-6,4-8"
+    
+    let actual = solve_a (sample |> String.to_reader |> Seq.from_reader) 
+                  
+    Assert.Equal(2, actual)
+    
+    
+[<Fact>]
+let ``solve_b with sample returns 4`` () =
+    let sample = "2-4,6-8\n\
+                  2-3,4-5\n\
+                  5-7,7-9\n\
+                  2-8,3-7\n\
+                  6-6,4-6\n\
+                  2-6,4-8"
+    
+    let actual = solve_b (sample |> String.to_reader |> Seq.from_reader) 
+                  
+    Assert.Equal(4, actual)
---
+++ fsharp/Problem04.fs
@@ -0,0 +1,32 @@
+﻿module Problem04
+
+
+let (|Split|_|) (c: char) (s: string) =
+    match s.Split c with
+    | [|a; b|] -> Some (a, b)
+    | _ -> None
+
+
+let parse = function
+    | Split ',' (Split '-' (a, b), Split '-' (c, d)) -> ((int a, int b), (int c, int d))
+    | _ -> failwith "Input string doesn't match with format a-b,c-d"
+    
+    
+let inside (a, b) (c, d) =
+    a >= c && b <= d
+
+
+let overlap (a, b) (c, d) =
+    a <= d && c <= b
+
+
+let solve_a (lines: string seq) =
+    lines |> Seq.map parse
+          |> Seq.filter (fun (r1, r2) -> inside r1 r2 || inside r2 r1)
+          |> Seq.length
+
+
+let solve_b (lines: string seq) =
+    lines |> Seq.map parse
+          |> Seq.filter (fun (r1, r2) -> overlap r1 r2)
+          |> Seq.length
