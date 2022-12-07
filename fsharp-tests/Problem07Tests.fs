3161a023c206f9a5754f30946655a31d21fa40c2
Mark Shevchenko <markshevchenko@gmail.com>
2022-12-07 17:34:10 +0300

    Solve problem 7 in F#

---
+++ fsharp-tests/Problem07Tests.fs
@@ -0,0 +1,45 @@
+﻿module Problem07Tests
+
+open Xunit
+open Problem07
+
+let sample = [
+    "$ cd /"
+    "$ ls"
+    "dir a"
+    "14848514 b.txt"
+    "8504156 c.dat"
+    "dir d"
+    "$ cd a"
+    "$ ls"
+    "dir e"
+    "29116 f"
+    "2557 g"
+    "62596 h.lst"
+    "$ cd e"
+    "$ ls"
+    "584 i"
+    "$ cd .."
+    "$ cd .."
+    "$ cd d"
+    "$ ls"
+    "4060174 j"
+    "8033020 d.log"
+    "5626152 d.ext"
+    "7214296 k" ]|> Seq.ofList
+
+
+[<Fact>]
+let ``solve_a with sample returns 95437`` () =
+    
+    let actual = solve_a sample
+                  
+    Assert.Equal(95_437UL, actual)
+    
+    
+[<Fact>]
+let ``solve_b with sample returns 24933642`` () =
+    
+    let actual = solve_b sample 
+                  
+    Assert.Equal(24_933_642UL, actual)
---
+++ fsharp/Problem07.fs
@@ -0,0 +1,87 @@
+﻿module Problem07
+
+open System
+open System.Collections.Generic
+open System.Text.RegularExpressions
+
+type File =
+    { name: string
+      size: uint64 }
+    
+type Directory =
+    { name: string
+      parent: Directory option
+      files: List<File>
+      directories: List<Directory> }
+
+let (|Cd|_|) parent s =
+    let m = Regex.Match(s, @"\$ cd (.+)")
+    if m.Success
+    then Some({ name = m.Groups[1].Value
+                parent = Some parent
+                files = List ()
+                directories = List () })
+    else None
+    
+let (|FileSize|_|) s =
+    let m = Regex.Match(s, @"(\d+) (.+)")
+    if m.Success
+    then Some( { name = m.Groups[2].Value
+                 size = UInt64.Parse m.Groups[1].Value })
+    else None
+
+let build_tree (lines: string seq) =
+    let mutable root = { name = "/"
+                         parent = None
+                         files = List ()
+                         directories = List() }
+    let mutable current_node = root
+    for line in lines do
+        match line with
+        | "$ cd /" -> ()
+        | "$ cd .." -> match current_node.parent with
+                       | Some parent -> current_node <- parent
+                       | None -> failwith "Unpair cd .. command"
+        | "$ ls" -> ()
+        | Cd current_node directory -> 
+                     current_node.directories.Add(directory)
+                     current_node <- directory
+        | FileSize file -> current_node.files.Add(file)
+        | _ -> ()
+        
+    root
+    
+    
+let get_directory_sizes root =
+    let rec collect directory: uint64 * uint64 list =
+        let sum_file_size = directory.files
+                         |> Seq.sumBy (fun f -> f.size)
+
+        let collects = directory.directories
+                    |> List.ofSeq
+                    |> List.map collect
+                 
+        let sum_dir_size = collects |> List.map fst |> List.sum
+        let total = collects |> List.map snd |> List.concat
+        let sum_size = sum_file_size + sum_dir_size
+        
+        (sum_size, sum_size::total)         
+        
+    collect root |> snd |> Seq.ofList
+
+    
+let solve_a (lines: string seq) =
+    let root = build_tree lines
+    let sizes = get_directory_sizes root
+    sizes |> Seq.filter ((>=) 100_000UL) 
+          |> Seq.sum
+
+
+let solve_b (lines: string seq) =
+    let root = build_tree lines
+    let sizes = get_directory_sizes root
+    let used = Seq.max sizes
+    let unused = 70_000_000UL - used
+    let need_free = 30_000_000UL - unused
+    sizes |> Seq.sort
+          |> Seq.find ((<) need_free)
