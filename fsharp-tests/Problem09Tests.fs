edbd05c5ae72572ba24fa37fa487d0d2872093a0
Mark Shevchenko <markshevchenko@gmail.com>
2022-12-09 15:06:28 +0300

    Solve problem 9 in F#

--- fsharp-tests/Problem09Tests.fs
+++ fsharp-tests/Problem09Tests.fs
@@ -3,6 +3,56 @@
 open Xunit
 open Problem09
 
+[<Fact>]
+let ``move should return valid values`` () =
+    Assert.Equal ({ row = 1; column = 0 }, move Up { row = 0; column = 0 })
+    Assert.Equal ({ row = 0; column = 1 }, move Right { row = 0; column = 0 })
+    Assert.Equal ({ row = -1; column = 0 }, move Down { row = 0; column = 0 })
+    Assert.Equal ({ row = 0; column = -1 }, move Left { row = 0; column = 0 })
+
+    
+[<Fact>]
+let ``move_tail should move tail valid`` () =
+    let head = ({ row = 0; column = 0 })
+    let offset row column position =
+        { row = position.row + row; column = position.column + column }
+    let check_offset row column = move_tail head (offset row column head)
+    
+    Assert.Equal ({ row = -1; column = -1 }, check_offset -1 -1)
+    Assert.Equal ({ row = -1; column = 0 }, check_offset -1 0)
+    Assert.Equal ({ row = -1; column = 1 }, check_offset -1 1)
+    Assert.Equal ({ row = 0; column = -1 }, check_offset 0 -1)
+    Assert.Equal ({ row = 0; column = 0 }, check_offset 0 0)
+    Assert.Equal ({ row = 0; column = 1 }, check_offset 0 1)
+    Assert.Equal ({ row = 1; column = -1 }, check_offset 1 -1)
+    Assert.Equal ({ row = 1; column = 0 }, check_offset 1 0)
+    Assert.Equal ({ row = 1; column = 1 }, check_offset 1 1)
+    Assert.Equal ({ row = -1; column = -1 }, check_offset -2 -2)
+    Assert.Equal ({ row = -1; column = 0 }, check_offset -2 -1)
+    Assert.Equal ({ row = -1; column = 0 }, check_offset -2 0)
+    Assert.Equal ({ row = -1; column = 0 }, check_offset -2 1)
+    Assert.Equal ({ row = -1; column = 1 }, check_offset -2 2)
+    Assert.Equal ({ row = 1; column = -1 }, check_offset 2 -2)
+    Assert.Equal ({ row = 1; column = 0 }, check_offset 2 -1)
+    Assert.Equal ({ row = 1; column = 0 }, check_offset 2 0)
+    Assert.Equal ({ row = 1; column = 0 }, check_offset 2 1)
+    Assert.Equal ({ row = 1; column = 1 }, check_offset 2 2)
+    Assert.Equal ({ row = 0; column = -1 }, check_offset -1 -2)
+    Assert.Equal ({ row = 0; column = -1 }, check_offset 0 -2)
+    Assert.Equal ({ row = 0; column = -1 }, check_offset 1 -2)
+    Assert.Equal ({ row = 0; column = 1 }, check_offset -1 2)
+    Assert.Equal ({ row = 0; column = 1 }, check_offset 0 2)
+    Assert.Equal ({ row = 0; column = 1 }, check_offset 1 2)
+
+
+[<Fact>]
+let ``parse should split line to direction and integer`` () =
+    Assert.Equal ((Up, 4), parse "U 4")
+    Assert.Equal ((Right, 12), parse "R 12")
+    Assert.Equal ((Down, 5), parse "D 5")
+    Assert.Equal ((Left, 1), parse "L 1")
+
+
 let sample = [
     "R 4"
     "U 4"
@@ -22,7 +72,25 @@ let ``solve_a with sample returns 13`` () =
     
     
 [<Fact>]
-let ``solve_b with sample returns 8`` () =
+let ``solve_b with sample returns 1`` () =
     let actual = solve_b sample 
                   
-    Assert.Equal (8, actual)
+    Assert.Equal (1, actual)
+
+
+let sample2 = [
+    "R 5"
+    "U 8"
+    "L 8"
+    "D 3"
+    "R 17"
+    "D 10"
+    "L 25"
+    "U 20" ]
+
+
+[<Fact>]
+let ``solve_b with sample2 returns 36`` () =
+    let actual = solve_b sample2 
+                  
+    Assert.Equal (36, actual)
--- fsharp/Problem09.fs
+++ fsharp/Problem09.fs
@@ -3,16 +3,65 @@
 type Position = { row: int
                   column: int }
 
-let up position = { position with row = position.row + 1 }
-let right position = { position with column = position.column + 1 }
-let down position = { position with row = position.row - 1 }
-let left position = { position with column = position.column - 1 }
 
-let are_near position1 position2 =
-    abs (position1.row - position2.row) < 2 && abs (position1.column - position2.column) < 2
+type Direction = Up | Right | Down | Left
+
+
+let move direction position =
+    match direction with
+    | Up -> { position with row = position.row + 1 }
+    | Right -> { position with column = position.column + 1 }
+    | Down -> { position with row = position.row - 1 }
+    | Left -> { position with column = position.column - 1 }
+
+
+let move_tail head tail =
+    match tail.row - head.row, tail.column - head.column with
+    | 2, 2 -> head |> move Up |> move Right
+    | 2, -2 -> head |> move Up |> move Left
+    | -2, 2 -> head |> move Down |> move Right
+    | -2, -2 -> head |> move Down |> move Left
+    | 2, _ -> move Up head
+    | -2, _ -> move Down head
+    | _, 2 -> move Right head
+    | _, -2 -> move Left head
+    | _ -> tail
+    
+    
+let parse (line: string) =
+    let direction =
+        match line[0] with
+        | 'U' -> Up
+        | 'R' -> Right
+        | 'D' -> Down
+        | 'L' -> Left
+        | c -> failwith $"Invalid direction character %c{c}"
+    let n = line.Substring (2) |> int
+    (direction, n)
+
 
 let solve_a (lines: string seq) =
-    0
+    let mutable head = { row = 0; column = 0 }
+    let mutable tail = { row = 0; column = 0 }
+    let mutable tails = Set<_> (seq { yield tail })
+    for line in lines do
+        let (direction, n) = parse line
+        for _ in 1..n do
+            head <- move direction head
+            tail <- move_tail head tail
+            tails <- tails.Add tail
+            
+    tails.Count
 
 let solve_b (lines: string seq) =
-    0
+    let mutable rope = Array.replicate 10 { row = 0; column = 0 }
+    let mutable tails = Set<_> (seq { yield Array.last rope })
+    for line in lines do
+        let (direction, n) = parse line
+        for _ in 1..n do
+            rope[0] <- move direction rope[0]
+            for i in 1..rope.Length - 1 do
+                rope[i] <- move_tail rope[i - 1] rope[i]
+            tails <- tails.Add (Array.last rope)
+            
+    tails.Count
