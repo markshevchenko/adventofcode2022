a89ac843594c24cfbfd1e34fe937ebe6a7061f45
Mark Shevchenko <markshevchenko@gmail.com>
2022-12-09 09:23:46 +0300

    Start solve problem 9 in F#

---
+++ fsharp-tests/Problem09Tests.fs
@@ -0,0 +1,28 @@
+﻿module Problem09Tests
+
+open Xunit
+open Problem09
+
+let sample = [
+    "R 4"
+    "U 4"
+    "L 3"
+    "D 1"
+    "R 4"
+    "D 1"
+    "L 5"
+    "R 2" ]
+
+
+[<Fact>]
+let ``solve_a with sample returns 13`` () =
+    let actual = solve_a sample
+                  
+    Assert.Equal (13, actual)
+    
+    
+[<Fact>]
+let ``solve_b with sample returns 8`` () =
+    let actual = solve_b sample 
+                  
+    Assert.Equal (8, actual)
---
+++ fsharp/Problem09.fs
@@ -0,0 +1,18 @@
+﻿module Problem09
+
+type Position = { row: int
+                  column: int }
+
+let up position = { position with row = position.row + 1 }
+let right position = { position with column = position.column + 1 }
+let down position = { position with row = position.row - 1 }
+let left position = { position with column = position.column - 1 }
+
+let are_near position1 position2 =
+    abs (position1.row - position2.row) < 2 && abs (position1.column - position2.column) < 2
+
+let solve_a (lines: string seq) =
+    0
+
+let solve_b (lines: string seq) =
+    0
