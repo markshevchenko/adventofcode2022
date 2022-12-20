c7bc513c9031037779fdffc70045697098534b4e
Mark Shevchenko <markshevchenko@gmail.com>
2022-12-20 19:05:55 +0300

    Continue to solve problem 16 in Rust

--- rust/src/problem16.rs
+++ rust/src/problem16.rs
@@ -1,70 +1,95 @@
-use std::collections::{HashSet, HashMap};
-use std::hash::{Hash, Hasher};
+use std::collections::{HashMap};
 use lazy_static::lazy_static;
 use regex::Regex;
 
-struct Graph {
-    rates: Vec<u32>,
-    leads: Vec<Vec<usize>>,
+#[derive(Clone)]
+struct Valve {
+    rate: u32,
+    tunnels: Vec<usize>,
+    distances: HashMap<usize, usize>,
 }
 
-impl Graph {
-    fn parse_line(line: &String) -> (String, u32, Vec<String>) {
-        lazy_static! {
-            static ref REGEX: Regex = Regex::new(r"^Valve (\w\w) has flow rate=(\d+); tunnels? leads? to valves? (.*)$").unwrap();
+fn parse_line(line: String) -> (String, u32, Vec<String>) {
+    lazy_static! {
+        static ref REGEX: Regex = Regex::new(r"^Valve (\w\w) has flow rate=(\d+); tunnels? leads? to valves? (.*)$").unwrap();
+    }
+
+    let captures = REGEX.captures(&line).unwrap();
+    let name = captures[1].to_string();
+    let rate = captures[2].parse::<u32>().unwrap();
+    let names = captures[3].split(", ").map(|s| s.to_string()).collect::<Vec<String>>();
+
+    (name, rate, names)
+}
+
+fn parse_valves(lines: &mut dyn Iterator<Item=String>) -> Vec<Valve> {
+    let descriptors: Vec<_> = lines.map(parse_line).collect();
+    let mut indexes = HashMap::new();
+    for index in 0..descriptors.len() {
+        let name = descriptors[index].0.clone();
+        indexes.insert(name, index);
+    }
+
+    let mut result = Vec::new();
+    for descriptor in descriptors.iter() {
+        let rate = descriptor.1;
+
+        let mut tunnels = Vec::new();
+        for name in &descriptor.2 {
+            let index = indexes[name];
+            tunnels.push(index);
         }
 
-        let captures = REGEX.captures(line).unwrap();
-        let name = captures[1].to_string();
-        let rate = captures[2].parse::<u32>().unwrap();
-        let names = captures[3].split(", ").map(|s| s.to_string()).collect::<Vec<String>>();
-
-        (name, rate, names)
+        result.push(Valve { rate, tunnels, distances: HashMap::new() });
     }
-    
-    fn parse(lines: &mut dyn Iterator<Item=String>) -> Self {
-        // let mut indexes = HashMap::new();
-        let mut rates = Vec::new();
-        let mut leads = Vec::new();
-        
-        // for line in lines {
-        //     let (name, rate, names) = Graph::parse_line(&line);
-        //     let index = if indexes.contains_key(&name) {
-        //         indexes[&name]
-        //     } else {
-        //         let index = indexes.len();
-        //         indexes.insert(&name, index);
-        //
-        //         index
-        //     };
-        //
-        //     let mut lead_indexes = Vec::new();
-        //
-        //     for name in names {
-        //         let index = if indexes.contains_key(&name) {
-        //             indexes[&name]
-        //         } else {
-        //             let index = indexes.len();
-        //             indexes.insert(&name, index);
-        //
-        //             index
-        //         };
-        //
-        //         lead_indexes.push(index);
-        //     }
-        //
-        //     rates.push(rate);
-        //     leads.push(lead_indexes);
-        // }
 
-        Graph { rates, leads }
+    for index in 0..result.len() {
+        let mut distances = HashMap::new();
+        build_distance_matrix(&result, index, 1, &result[index].tunnels, &mut distances);
+        result[index].distances = distances;
     }
+
+    result
+}
+
+fn build_distance_matrix(valves: &[Valve], index: usize, distance: usize, columns: &[usize], row: &mut HashMap<usize, usize>) {
+    let mut next_columns = Vec::new();
+    for column in columns {
+        if *column == index {
+            continue;
+        }
+
+        if row.contains_key(&column) {
+            continue;
+        }
+
+        row.insert(*column, distance);
+
+        for tunnel in &valves[*column].tunnels {
+            next_columns.push(*tunnel);
+        }
+    }
+
+    if next_columns.is_empty() {
+        return;
+    }
+
+    build_distance_matrix(valves, index, distance + 1, &next_columns, row);
 }
 
 pub fn solve_a(lines: &mut dyn Iterator<Item=String>) -> usize {
-    let graph = Graph::parse(lines);
-    
-    
+    let valves = parse_valves(lines);
+
+    for i in 0..valves.len() {
+        print!("Index {}, Rate {},", i, valves[i].rate);
+
+        for distance in valves[i].distances.iter() {
+            print!("[{}, {}]", distance.0, distance.1);
+        }
+
+        println!("");
+    }
+
     0
 }
 
@@ -90,7 +115,7 @@ fn solve_a_with_sample_data_returns_1651() {
 }
 
 pub fn solve_b(lines: &mut dyn Iterator<Item=String>) -> usize {
-    let _graph = Graph::parse(lines);
+    let _valves = parse_valves(lines);
     0
 }
 
