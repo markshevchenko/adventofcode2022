7854fcaa0eee84a4d01fdc423f6a9bbae8f2f774
Mark Shevchenko <markshevchenko@gmail.com>
2022-12-21 10:26:23 +0300

    Solve problem 21 in F#

---
+++ fsharp-tests/Problem21Tests.fs
@@ -0,0 +1,40 @@
+﻿module Problem21Tests
+
+open Xunit
+open Problem21
+
+[<Fact>]
+let ``parse should parse lines`` () =
+    Assert.Equal ((Name "cczh", Operation (Add, Name "sllz", Name "lgvd")), parse "cczh: sllz + lgvd")
+    Assert.Equal ((Name "dvpt", Number 3), parse "dvpt: 3")
+
+let sample = [
+    "root: pppw + sjmn"
+    "dbpl: 5"
+    "cczh: sllz + lgvd"
+    "zczc: 2"
+    "ptdq: humn - dvpt"
+    "dvpt: 3"
+    "lfqf: 4"
+    "humn: 5"
+    "ljgn: 2"
+    "sjmn: drzm * dbpl"
+    "sllz: 4"
+    "pppw: cczh / lfqf"
+    "lgvd: ljgn * ptdq"
+    "drzm: hmdt - zczc"
+    "hmdt: 32" ]
+
+
+[<Fact>]
+let ``solve_a with sample returns 152`` () =
+    let actual = solve_a sample
+                  
+    Assert.Equal (152L, actual)
+    
+    
+[<Fact>]
+let ``solve_b with sample returns 301`` () =
+    let actual = solve_b sample 
+                  
+    Assert.Equal (301L, actual)
---
+++ fsharp/Problem21.fs
@@ -0,0 +1,93 @@
+﻿module Problem21
+
+open System
+open System.Collections.Generic
+
+type Name = Name of string
+type Operation =
+    | Add
+    | Sub
+    | Mul
+    | Div
+    
+type Operand = Left | Right
+
+type Expression =
+    | Number of int64
+    | Operation of Operation * Name * Name
+    
+type Reactive =
+    | Result of int64
+    | Operations of (Operation * Operand * int64) list
+    
+let parse (line: string) =
+    let name = Name line[0..3]
+    if Char.IsDigit line[6]
+    then (name, Number (int64 line[6..]))
+    else let left = Name line[6..9]
+         let right = Name line[13..16]
+         let operation = match line[11] with
+                         | '+' -> Add
+                         | '-' -> Sub
+                         | '*' -> Mul
+                         | '/' -> Div
+                         | _ -> failwith $"Unknown operation {line[11]}"
+         (name, Operation (operation, left, right))
+         
+let rec calculate (steps: Dictionary<Name, Expression>) (name: Name) =
+    match steps[name] with
+    | Number number -> number
+    | Operation (operation, left, right) ->
+        let left_value = calculate steps left
+        let right_value = calculate steps right
+        match operation with
+        | Add -> left_value + right_value
+        | Sub -> left_value - right_value
+        | Mul -> left_value * right_value
+        | Div -> left_value / right_value
+        
+let solve_a (lines: string seq) =
+    let mutable steps = Dictionary<Name, Expression> ()
+    for line in lines do
+        let name, expression = parse line
+        steps.Add (name, expression)
+        
+    calculate steps (Name "root")
+    
+let rec reactive_build (steps: Dictionary<Name, Expression>) (name: Name) =
+    if name = Name "humn"
+    then Operations []
+    else match steps[name] with
+         | Number number -> Result number
+         | Operation (operation, left, right) ->
+            let left_value = reactive_build steps left
+            let right_value = reactive_build steps right
+            match (operation, left_value, right_value) with
+            | Add, Result a, Result b -> Result (a + b)
+            | Sub, Result a, Result b -> Result (a - b)
+            | Mul, Result a, Result b -> Result (a * b)
+            | Div, Result a, Result b -> Result (a / b)
+            | operation, Result a, Operations b -> Operations ((operation, Left, a)::b)
+            | operation, Operations a, Result b -> Operations ((operation, Right, b)::a)
+            | _ -> failwith "Too complicated to solve"
+            
+let rec reactive_calculate (expect: int64) (operations: (Operation * Operand * int64) list) =            
+    match operations with
+    | [] -> expect
+    | (Add, _, value)::tail_operations -> reactive_calculate (expect - value) tail_operations
+    | (Sub, Left, value)::tail_operations -> reactive_calculate (value - expect) tail_operations
+    | (Sub, Right, value)::tail_operations -> reactive_calculate (expect + value) tail_operations
+    | (Mul, _, value)::tail_operations -> reactive_calculate (expect / value) tail_operations
+    | (Div, Left, value)::tail_operations -> reactive_calculate (value / expect) tail_operations
+    | (Div, Right, value)::tail_operations -> reactive_calculate (expect * value) tail_operations
+            
+
+let solve_b (lines: string seq) =
+    let mutable steps = Dictionary<Name, Expression> ()
+    for line in lines do
+        let name, expression = parse line
+        steps.Add (name, expression)
+
+    match reactive_build steps (Name "root") with
+    | Operations ((_, _, expect)::operations) -> reactive_calculate expect operations
+    | _ -> failwith "Invalid reactive expression"
